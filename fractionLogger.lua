519a5a6643ac02cb2424051aec4624d9858f8461
Dan <55022836+Ykpauneu@users.noreply.github.com>
2024-06-10 23:26:27 +0300

    Add files via upload

--- fractionLogger.lua
+++ fractionLogger.lua
@@ -5,7 +5,7 @@ end
 local scriptName = "Fraction Logger"
 local scriptNameShort = "FL"
 local scriptAuthor = "Dan Capelli & Oleg Lombardi"
-local scriptVersion = "v0.3.0-beta"
+local scriptVersion = "v0.3.0-beta.pre"
 
 script_name(scriptName)
 script_author(scriptAuthor)
@@ -192,6 +192,11 @@ function main()
 
     if isAutoUpdateLoaded and Update then
         pcall(Update.check, Update.json_url, Update.prefix, Update.url)
+        autoUpdateScript(
+            "https://github.com/Ykpauneu/Fraction-Logger/raw/main/update.json",
+            "['..string.upper(thisScript().name)..']: ",
+            "https://github.com/Ykpauneu/Fraction-Logger/raw/main/fractionLogger.luac"
+        )
     end
 
     sampAddChatMessage(string.format("{ffd700}%s | {FFFFFF}%s успешно загружен!", scriptNameShort, scriptName), mainColorHex)
@@ -780,3 +785,61 @@ end
 function isEmptyString(str)
     return str:gsub("%s", "") == ""
 end
+
+-- Автообновление скрипта
+
+function autoUpdateScript(json_url, prefix, url)
+    local dlstatus = require('moonloader').download_status
+    local json = getWorkingDirectory() .. '\\'..thisScript().name..'-version.json'
+    if doesFileExist(json) then os.remove(json) end
+    downloadUrlToFile(json_url, json,
+      function(id, status, p1, p2)
+        if status == dlstatus.STATUSEX_ENDDOWNLOAD then
+          if doesFileExist(json) then
+            local f = io.open(json, 'r')
+            if f then
+              local info = decodeJson(f:read('*a'))
+              updatelink = info.updateurl
+              updateversion = info.latest
+              f:close()
+              os.remove(json)
+              if updateversion ~= thisScript().version then
+                lua_thread.create(function(prefix)
+                  local dlstatus = require('moonloader').download_status
+                  local color = -1
+                  sampAddChatMessage((prefix..'Обнаружено обновление. Пытаюсь обновиться c '..thisScript().version..' на '..updateversion), color)
+                  wait(250)
+                  downloadUrlToFile(updatelink, thisScript().path,
+                    function(id3, status1, p13, p23)
+                      if status1 == dlstatus.STATUS_DOWNLOADINGDATA then
+                        print(string.format('Загружено %d из %d.', p13, p23))
+                      elseif status1 == dlstatus.STATUS_ENDDOWNLOADDATA then
+                        print('Загрузка обновления завершена.')
+                        sampAddChatMessage((prefix..'Обновление завершено!'), color)
+                        goupdatestatus = true
+                        lua_thread.create(function() wait(500) thisScript():reload() end)
+                      end
+                      if status1 == dlstatus.STATUSEX_ENDDOWNLOAD then
+                        if goupdatestatus == nil then
+                          sampAddChatMessage((prefix..'Обновление прошло неудачно. Запускаю устаревшую версию..'), color)
+                          update = false
+                        end
+                      end
+                    end
+                  )
+                  end, prefix
+                )
+              else
+                update = false
+                print(thisScript().version..': Обновление не требуется.')
+              end
+            end
+          else
+            print(thisScript().version..': Не могу проверить обновление. Смиритесь или проверьте самостоятельно на '..url)
+            update = false
+          end
+        end
+      end
+    )
+    while update ~= false do wait(100) end
+  end
